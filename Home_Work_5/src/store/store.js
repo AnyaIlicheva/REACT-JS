21dae7ed5d6bd4e5705c60be1bdaf28d1199aff8
Anna Ilicheva <161249642+AnyaIlicheva@users.noreply.github.com>
2024-09-29 17:35:21 +0300

    Update store.js

--- Home_Work_5/src/store/store.js
+++ Home_Work_5/src/store/store.js
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userReducer";
 import themeReducer from "./themeReducer";
 
-//Создание хранилища (store)
 export const store = configureStore({
   reducer: {
     user: userReducer,
